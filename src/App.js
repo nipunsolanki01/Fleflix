2dd136b5d081b255b03c9f3243a942b075aa7bf3
Nipun <nipun2001860@st.jmi.ac.in>
2023-08-22 20:48:01 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Row from './Row'
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
+function App() {
+  return (
+    <div className="app">
+      <Nav/>
+      <Banner/>
+      <Row
+        title="NETFLIX ORIGINALS" 
+        isLargeRow={true}
+        fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+    </div>
+  );
+}
+
+export default App;
